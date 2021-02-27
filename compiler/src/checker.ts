33ab50f77fcfc0f0290e76240a8accc44b9a3564
Sam Vervaeck <samvv@pm.me>
2021-02-27 11:30:53 +0100

    compiler: Add better support for recursive functions

--- compiler/src/checker.ts
+++ compiler/src/checker.ts
@@ -1,4 +1,5 @@
 
+import { DefaultDeserializer } from "v8";
 import { BoltModifiers, BoltPattern, BoltVariableDeclaration, isBoltAssignStatement, isBoltBlockExpression, isBoltExpression, isBoltFunctionDeclaration, isBoltFunctionExpression, isBoltSourceFile, isBoltVariableDeclaration, kindToString, SourceFile, Syntax, SyntaxKind } from "./ast";
 import { getAllReturnStatementsInFunctionBody, getSymbolText } from "./common";
 import { assert, FastStringMap } from "./util";
@@ -193,7 +194,11 @@ class TypeVarSubstitution {
     return this.mapping.has(source.varId);
   }
 
-  public compose(other: TypeVarSubstitution): TypeVarSubstitution {
+  public delete(source: TypeVar): void {
+    this.mapping.delete(source.varId);
+  }
+
+  public composeWith(other: TypeVarSubstitution): TypeVarSubstitution {
     const newSubstitution = new TypeVarSubstitution();
     for (const [typeVar, type] of other) {
       newSubstitution.add(typeVar, type.applySubstitution(this));
@@ -202,6 +207,15 @@ class TypeVarSubstitution {
       newSubstitution.add(typeVar, type);
     }
     return newSubstitution;
+  }
+
+  public applyComposition(other: TypeVarSubstitution): void {
+    for (const [typeVar, type] of other) {
+      // if (this.has(typeVar)) {
+      //   this.delete(typeVar);
+      // }
+      this.add(typeVar, type.applySubstitution(this));
+    }
   }
 
   public defaults(other: TypeVarSubstitution): void{ 
@@ -297,6 +311,10 @@ export class TypeEnv {
 
   private mapping = new FastStringMap<string, Scheme>();
 
+  constructor(public parentTypeEnv: TypeEnv | null = null) {
+
+  }
+
   public set(name: string, scheme: Scheme) {
     this.mapping.set(name, scheme)
   }
@@ -307,13 +325,27 @@ export class TypeEnv {
 
   public lookup(name: string): Type | null {
 
-    // Check if the binding exists. It's the caller's responsibility to so
-    // something sensible if the binding was not found.
-    if (!this.mapping.has(name)) {
-      return null;
-    }
+    let scheme;
+    let currEnv: TypeEnv | null = this;
 
-    const scheme = this.mapping.get(name);
+    while (true) { 
+
+      // Check if the binding exists. It's the caller's responsibility to so
+      // something sensible if the binding was not found.
+      if (currEnv!.mapping.has(name)) {
+        scheme = currEnv.mapping.get(name);
+        break;
+      }
+
+      currEnv = currEnv.parentTypeEnv;
+
+      // If we went through every parent environment and still did not found a
+      // valid type, then we return nothing.
+      if (currEnv === null) {
+        return null;
+      }
+
+    }
 
     // Now we instantiate the type inside the scheme with some fresh variables
     // that are guaranteed to not occur anywhere else.
@@ -325,11 +357,20 @@ export class TypeEnv {
   }
 
   public has(typeVar: TypeVar): boolean {
-    return this.mapping.has(typeVar.varId)
+    let currEnv: TypeEnv | null = this;
+    while (true) { 
+      if (currEnv!.mapping.has(typeVar.varId)) {
+        return true;
+      }
+      currEnv = currEnv.parentTypeEnv;
+      if (currEnv === null) {
+        return false;
+      }
+    }
   }
 
   public clone(): TypeEnv {
-    const result = new TypeEnv();
+    const result = new TypeEnv(this.parentTypeEnv);
     for (const [name, scheme] of this.mapping) {
       result.set(name, scheme);
     }
@@ -338,11 +379,15 @@ export class TypeEnv {
 
   public getFreeVariables(): TypeVarSet {
     const freeVariables = new TypeVarSet();
-    for (const scheme of this.mapping.values()) {
-      for (const typeVar of scheme.getFreeVariables()) {
-        freeVariables.add(typeVar);
+    let currEnv: TypeEnv | null = this;
+    do {
+      for (const scheme of currEnv.mapping.values()) {
+        for (const typeVar of scheme.getFreeVariables()) {
+          freeVariables.add(typeVar);
+        }
       }
-    }
+      currEnv = currEnv!.parentTypeEnv;
+    } while (currEnv !== null);
     return freeVariables;
   }
 
@@ -449,6 +494,10 @@ export class TypeChecker {
     return type === this.stringType;
   }
 
+  public isBoolType(type: Type) {
+    return type === this.boolType;
+  }
+
   private builtinTypes = new FastStringMap<string, Type>([
     ['int', this.intType],
     ['String', this.stringType],
@@ -463,16 +512,9 @@ export class TypeChecker {
     const newTypeEnv = new TypeEnv();
     this.nodeToTypeEnv.set(sourceFile.id, newTypeEnv)
     const constraints: Constraint[] = [];
+    this.forwardDeclare(sourceFile, newTypeEnv);
     this.checkNode(sourceFile, newTypeEnv, constraints);
     this.sourceFileToSubstitution.set(sourceFile.id, this.solveConstraints(constraints));
-  }
-
-  private applySubstitutionToConstraints(constraints: Constraint[], substitution: TypeVarSubstitution): void {
-    for (let i = 0; i < constraints.length; i++) {
-      const constraint = constraints[i];
-      constraint[0] = constraint[0].applySubstitution(substitution)
-      constraint[1] = constraint[1].applySubstitution(substitution)
-    }
   }
 
   private inferBinding(
@@ -522,13 +564,95 @@ export class TypeChecker {
     }
   }
 
+  private forwardDeclare(node: Syntax, typeEnv: TypeEnv) {
+
+    switch (node.kind) {
+
+      case SyntaxKind.BoltSourceFile:
+        {
+          for (const element of node.elements) {
+            this.forwardDeclare(element, typeEnv);
+          }
+          break;
+        }
+
+      case SyntaxKind.BoltReferenceExpression:
+      case SyntaxKind.BoltConstantExpression:
+        break;
+
+      case SyntaxKind.BoltCallExpression:
+        {
+          this.forwardDeclare(node.operator, typeEnv);
+          for (const operand of node.operands) {
+            this.forwardDeclare(operand, typeEnv);
+          }
+          break;
+        }
+
+      case SyntaxKind.BoltFunctionExpression:
+        {
+          // TODO
+          break;
+        }
+
+      case SyntaxKind.BoltExpressionStatement:
+        {
+          this.forwardDeclare(node.expression, typeEnv);
+          break;
+        }
+
+      case SyntaxKind.BoltVariableDeclaration:
+        break;
+
+      case SyntaxKind.BoltAssignStatement:
+        {
+          this.forwardDeclare(node.rhs, typeEnv);
+          break;
+        }
+
+      case SyntaxKind.BoltFunctionDeclaration:
+        {
+
+          const name = getSymbolText(node.name);
+
+          // Right now, we don't know much about the function and just want to
+          // make sure that whatever we learn about it is linked into this type variable.
+          const fnType = new TypeVar();
+
+          this.nodeToType.set(node.id, fnType);
+
+          // We get/create a type environment that is NOT cloned, because that
+          // allows us complete access to all locally declared identifiers inside
+          // the function.
+          const innerTypeEnv = this.getTypeEnvForNode(node, typeEnv, false);
+
+          // Register this function in the type environment it was presumably
+          // defined in. This will also define it in the function's body since
+          // the two environments should be linked.
+          typeEnv.set(name, new ForallScheme(new TypeVarSet(), fnType));
+
+          break;
+        }
+
+      default:
+        throw new Error(`Could not forward declare node: unkown node type`);
+
+    }
+
+  }
+
   private inferNode(node: Syntax, typeEnv: TypeEnv, constraints: Constraint[]): Type {
 
     switch (node.kind) {
 
       case SyntaxKind.BoltMatchExpression:
         {
+
+          // We get/create a type environment that is NOT cloned, because that
+          // allows us complete access to all locally declared identifiers inside
+          // the function.
           const resultType = new TypeVar();
+
           for (const matchArm of node.arms) {
             const type = this.inferNode(matchArm.body, typeEnv, constraints);
             // FIXME Should let-bindings generated in a match arm be generalised?
@@ -538,6 +662,7 @@ export class TypeChecker {
               resultType
             ])
           }
+
           return resultType;
         }
 
@@ -554,10 +679,13 @@ export class TypeChecker {
       case SyntaxKind.BoltFunctionDeclaration:
         {
 
-          const name = getSymbolText(node.name);
+          // By now, the forward declaration has made sure that the function
+          // body's environment already exists. It should be a fork of the
+          // parent environment and not simply a clone.
+          const innerTypeEnv = this.getTypeEnvForNode(node, typeEnv, false)
 
-          // This environment will be used inside the function body. It is created only once.
-          const innerTypeEnv = typeEnv.clone();
+          // Get our type that was forward-declared.
+          const fnType = this.nodeToType.get(node.id);
 
           const paramTypes: Type[] = [];
           for (const param of node.params) {
@@ -567,13 +695,10 @@ export class TypeChecker {
           }
 
           const returnType = new TypeVar();
-          const fnType = new ArrowType(paramTypes, returnType);
-
-          // Register this function in the type environment it was presumably
-          // defined in. Currently, we must do this twice because there is no
-          // way to make one environment the 'parent' of another environment.
-          typeEnv.set(name, new ForallScheme(new TypeVarSet(), fnType));
-          innerTypeEnv.set(name, new ForallScheme(new TypeVarSet(), fnType));
+          constraints.push([
+            fnType,
+            new ArrowType(paramTypes, returnType)
+          ]);
 
           if (node.returnTypeExpr !== null) {
             constraints.push([
@@ -586,7 +711,9 @@ export class TypeChecker {
             for (const returnStmt of getAllReturnStatementsInFunctionBody(node.body)) {
               constraints.push([
                 returnType,
-                returnStmt.value === null ? this.voidType : this.inferNode(returnStmt.value, innerTypeEnv, constraints)
+                returnStmt.value === null
+                  ? this.voidType
+                  : this.inferNode(returnStmt.value, innerTypeEnv, constraints)
               ]);
             }
           }
@@ -624,6 +751,7 @@ export class TypeChecker {
             // problems in the case we assign an expression of a different type
             // to the variable. The assignment would be accepted, while it really
             // shouldn't.
+            // See also https://cstheory.stackexchange.com/a/42557
             this.inferBinding(node.bindings, typeVar, typeEnv, constraints, false);
 
             resultType = typeVar;
@@ -660,13 +788,19 @@ export class TypeChecker {
       case SyntaxKind.BoltReferenceExpression:
         {
           const varName = getSymbolText(node.name.name);
+          if (varName === 'true') {
+            return this.boolType;
+          }
+          if (varName === 'false') {
+            return this.boolType;
+          }
           if (varName === '+') {
             return new ArrowType([ this.intType, this.intType ], this.intType);
           }
           if (varName === '-') {
             return new ArrowType([ this.intType, this.intType ], this.intType);
           }
-          if (varName === '-') {
+          if (varName === '/') {
             return new ArrowType([ this.intType, this.intType ], this.intType);
           }
           if (varName === '*') {
@@ -791,21 +925,33 @@ export class TypeChecker {
       }
       const [a, b] = constraints.pop()!;
       const newSubstitution = this.unifies(a, b);
-      substitution = newSubstitution.compose(substitution);
-      this.applySubstitutionToConstraints(constraints, newSubstitution);
+      for (let i = 0; i < constraints.length; i++) {
+        const constraint = constraints[i];
+        constraint[0] = constraint[0].applySubstitution(newSubstitution)
+        constraint[1] = constraint[1].applySubstitution(newSubstitution)
+      }
+      substitution = newSubstitution.composeWith(substitution);
     }
   }
 
   private areTypesEqual(a: Type, b: Type): boolean {
+
+    // This is an early check that is not strictly necessary but might speed up
+    // things a bit.
     if (a === b) {
       return true;
     }
+
+    // We are checking if two types are equal syntactically, so if they are of
+    // a different kind we're already done.
     if (a.kind !== b.kind) {
       return false;
     }
+
     if (a.kind === TypeKind.PrimType && b.kind === TypeKind.PrimType) {
       return a.primId === b.primId;
     }
+
     if (a.kind === TypeKind.ArrowType && b.kind === TypeKind.ArrowType) {
       if (a.paramTypes.length !== b.paramTypes.length
           || !this.areTypesEqual(a.returnType, b.returnType)) {
@@ -818,47 +964,79 @@ export class TypeChecker {
       }
       return true;
     }
+
     if (a.kind === TypeKind.TypeVar && b.kind === TypeKind.TypeVar) {
       return a.varId === b.varId;
     }
+
     throw new Error(`Unexpected combination of types while checking equality`)
   }
 
+  private unifyMany(a: Type[], b: Type[]): TypeVarSubstitution {
+    let substitution = new TypeVarSubstitution();
+    for (let i = 0; i < a.length; i++) {
+      const localSubstitution = this.unifies(a[i], b[i]);
+      for (let k = i; k < a.length; k++) {
+        a[k] = a[k].applySubstitution(localSubstitution);
+        b[k] = b[k].applySubstitution(localSubstitution);
+      }
+      substitution = localSubstitution.composeWith(substitution);
+    }
+    return substitution;
+  }
+
   private unifies(a: Type, b: Type): TypeVarSubstitution {
+
+    // Two types that have the same structure can be unified as-is without
+    // requiring any substitution to take place.
     if (this.areTypesEqual(a, b)) {
       return new TypeVarSubstitution();
     }
+
+    // The following cases are quite straightforward. We perform some checks
+    // and return an empty substitution if both a and b point to the same type
+    // variable. Otherwise, we create a simple substitution for the type
+    // variable.
+
     if (a.kind === TypeKind.TypeVar) {
       return bindTypeVar(a, b);
     }
     if (b.kind === TypeKind.TypeVar) {
       return bindTypeVar(b, a);
     }
+
     if (a.kind === TypeKind.ArrowType && b.kind === TypeKind.ArrowType) {
+
+      // Right now, two arrow types must have the exact same amount of
+      // parameters or else an error occurs. In the future, this might changed
+      // in order to support default arguments.
       if (a.paramTypes.length !== b.paramTypes.length) {
         throw new ParamCountMismatchError(a, b);
       }
-      let substitution = new TypeVarSubstitution();
-      let returnA = a.returnType;
-      let returnB = b.returnType;
-      for (let i = 0; i < a.paramTypes.length; i++) {
-        const paramSubstitution = this.unifies(a.paramTypes[i], b.paramTypes[i]);
-        returnA = returnA.applySubstitution(paramSubstitution);
-        returnB = returnB.applySubstitution(paramSubstitution);
-        substitution = paramSubstitution.compose(substitution);
-      }
-      const returnSubstitution = this.unifies(returnA, returnB);
-      return returnSubstitution.compose(substitution);
+
+      // Becuase the return type depends on the parameter types, and because a
+      // parameter type may eventually depend on a previous parameter (due to
+      // default expressions), we use a unifier that simply runs from left to
+      // right.
+      return this.unifyMany(
+        [...a.paramTypes, a.returnType],
+        [...b.paramTypes, b.returnType]
+      );
     }
+
+    // If we got here then none of our unification rules matched, so the
+    // combination of types must be invalid.
     throw new UnificationError(a, b);
   }
 
-  private getTypeEnvForNode(node: Syntax): TypeEnv {
+  private getTypeEnvForNode(node: Syntax, parentTypeEnv: TypeEnv, shouldClone: boolean): TypeEnv {
     const scopeNode = getNodeIntroducingScope(node)
     if (this.nodeToTypeEnv.has(scopeNode.id)) {
       return this.nodeToTypeEnv.get(scopeNode.id)
     }
-    const newTypeEnv = new TypeEnv();
+    const newTypeEnv = shouldClone
+      ? parentTypeEnv.clone()
+      : new TypeEnv(parentTypeEnv);
     this.nodeToTypeEnv.set(scopeNode.id, newTypeEnv)
     return newTypeEnv;
   }
